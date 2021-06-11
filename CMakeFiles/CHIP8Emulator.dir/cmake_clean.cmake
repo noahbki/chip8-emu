0efc2080d2bc332bf748e53a5e86d4b919fef0e8
noahki <noah.keedle-isack@students.plymouth.ac.uk>
2021-06-11 22:57:40 +0100

    NA: Arbitrary stuff.

---
+++ CMakeFiles/CHIP8Emulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CHIP8Emulator"
+  "CHIP8Emulator.pdb"
+  "CMakeFiles/CHIP8Emulator.dir/src/Application.cpp.o"
+  "CMakeFiles/CHIP8Emulator.dir/src/Logger.cpp.o"
+  "CMakeFiles/CHIP8Emulator.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHIP8Emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
